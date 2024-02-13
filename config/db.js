f35fa4161c2707893471bc8c1714363d47cf6ab7
Zahurul Islam <78563080+zahuruli@users.noreply.github.com>
2024-02-13 10:56:44 +0600

    Database timeout issue fixed

--- config/db.js
+++ config/db.js
@@ -3,10 +3,14 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Add other options as needed
+    });
     console.log(`Successfully Connected to MongoDB DataBase`.bgMagenta.white);
   } catch (error) {
-    console.log(`Mongodb connection Failed ${error}`.bgRed.white);
+    console.log(`MongoDB connection failed: ${error.message}`.bgRed.white);
   }
 };
 
